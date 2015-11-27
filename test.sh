148e215e1325798aba0322dd5e32e31ea16972e7
Ruwan Dileepa <dileepa.ruwan@gmail.com>
2015-11-28 00:04:13 +0530

    added test.sh

---
+++ test.sh
@@ -0,0 +1,23 @@
+####################################################
+#                 Registration Form                #
+####################################################
+
+echo "First name :"
+read fname
+echo "Last Name : "
+read lname
+echo "Address : "
+read address
+echo "Age : "
+read age
+clear
+echo "###############################################"
+echo "#            Registration Form                #"
+echo "###############################################"
+
+echo "Full Name : $fname  $lname"
+echo "Age : $age"
+echo "Address : $address"
+
+
+
